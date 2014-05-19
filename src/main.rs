b085141f6f35006e34889ba2291fbd2c79987482
libfud <libfud@librehumanitas.org>
2014-05-19 11:22:00 -0400

    changed count_parens from fn to closure

--- src/main.rs
+++ src/main.rs
@@ -51,61 +51,52 @@ pub fn rust_add_history(line: &str) {
     });
 }
 
-/// Make sure there are equal amounts of parens and prevent malformed
-/// expressions such as ``(+ 2 2)(+ 2 2)"
-pub fn count_parens(expr: &str) -> (uint, uint, bool) {
-    let (mut lparens, mut rparens) = (0, 0);
-    for c in expr.chars() {
-        if c == '(' {
-            if lparens == rparens && lparens != 0 {
-                return (lparens, rparens, false)
-            } else {
-                lparens += 1
-            }
-        } else if c == ')' {
-            if rparens >= lparens && rparens != 0 {
-                return (lparens, rparens, false)
-            } else {
-                rparens += 1
-            }
-        }
-    }
-
-    (lparens, rparens, true)
-}
-
 fn main() {
     loop {
         let mut expr = match rust_linenoise(">>> ") {
             Some(val)   => { val.to_str() },
-            None    => { continue }
+            None    => { "".to_str() } //I hate ~str 
         };
 
-        let (mut lparens, mut rparens): (uint, uint);
-        //A valid expression can have no parens if and only if it is a
-        //constant or variable, eg 2, x, 'd' or "hello, world!"
         let mut okay_expr = true;
 
-        if expr.trim().starts_with("(") {
+        if expr.trim().starts_with("(") || expr.len() == 0 {
+            let count_parens = |expr: &str| -> (uint, uint, bool) {
+                let (mut lparens, mut rparens) = (0, 0);
+                for c in expr.chars() {
+                    if c == '(' {
+                        if lparens == rparens && lparens != 0 {
+                            return (lparens, rparens, false)
+                        } else {
+                            lparens += 1
+                        }
+                    } else if c == ')' {
+                        if rparens >= lparens && rparens != 0 {
+                            return (lparens, rparens, false)
+                        } else {
+                            rparens += 1
+                        }
+                    }
+                }
+
+                (lparens, rparens, true)
+            };
+
             loop {
-                let (lparenstmp, rparenstmp, okay_exprtmp) = count_parens(expr.as_slice());
-                lparens = lparenstmp;
-                rparens = rparenstmp;
-                okay_expr = okay_exprtmp;
-                if okay_expr == false { 
+                let (lparens, rparens, okay_expr_tmp) = count_parens(expr.as_slice());
+
+                if okay_expr_tmp == false {
+                    okay_expr = false;
+                    break
+                }
+                if lparens == rparens && lparens != 0 {
                     break
                 }
 
-                if lparens == rparens {
-                    okay_expr = true;
-                    break
-                }
-
-                 let expr_part = match rust_linenoise(" ") {
+                let expr_part = match rust_linenoise(" ") {
                     Some(val)   => { val.to_str() },
                     None    => { continue }
                 };
-
                 expr = expr + "\n" + expr_part;
             }
         }
@@ -114,7 +105,6 @@ fn main() {
             println!("Bad expression");
             continue
         }
-
         rust_add_history(expr);
 
         match expr.trim() {
