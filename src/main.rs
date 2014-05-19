55e5f8b197a8de019ed03a1f72b3d56cf47b3340
libfud <libfud@librehumanitas.org>
2014-05-19 09:04:22 -0400

    Multiple line input is now acceptable

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ extern crate libc;
 use libc::c_char;
 use std::c_str::CString;
 
+//pub mod read;
+
 #[link(name = "linenoise")]
 extern {
     fn linenoise(p: *c_char) -> *c_char;
@@ -16,8 +18,7 @@ extern {
 
 /// Attempts to read input from a user using linenoise. Returns an option,
 /// Some(StrBuf) for success, or None if EOF (^D) is entered.
-pub fn rust_linenoise(prompt: &str) -> Option<StrBuf>
-{
+pub fn rust_linenoise(prompt: &str) -> Option<StrBuf> {
     if prompt.len() == 0 {
         return None
     }
@@ -50,13 +51,70 @@ pub fn rust_add_history(line: &str) {
     });
 }
 
-fn main()
-{
+/// Make sure there are equal amounts of parens and prevent malformed
+/// expressions such as ``(+ 2 2)(+ 2 2)"
+pub fn count_parens(expr: &str) -> (uint, uint, bool) {
+    let (mut lparens, mut rparens) = (0, 0);
+    for c in expr.chars() {
+        if c == '(' {
+            if lparens == rparens && lparens != 0 {
+                return (lparens, rparens, false)
+            } else {
+                lparens += 1
+            }
+        } else if c == ')' {
+            if rparens >= lparens && rparens != 0 {
+                return (lparens, rparens, false)
+            } else {
+                rparens += 1
+            }
+        }
+    }
+
+    (lparens, rparens, true)
+}
+
+fn main() {
     loop {
-        let expr = match rust_linenoise(">>> ") {
+        let mut expr = match rust_linenoise(">>> ") {
             Some(val)   => { val.to_str() },
             None    => { continue }
         };
+
+        let (mut lparens, mut rparens): (uint, uint);
+        //A valid expression can have no parens if and only if it is a
+        //constant or variable, eg 2, x, 'd' or "hello, world!"
+        let mut okay_expr = true;
+
+        if expr.trim().starts_with("(") {
+            loop {
+                let (lparenstmp, rparenstmp, okay_exprtmp) = count_parens(expr.as_slice());
+                lparens = lparenstmp;
+                rparens = rparenstmp;
+                okay_expr = okay_exprtmp;
+                if okay_expr == false { 
+                    break
+                }
+
+                if lparens == rparens {
+                    okay_expr = true;
+                    break
+                }
+
+                 let expr_part = match rust_linenoise(" ") {
+                    Some(val)   => { val.to_str() },
+                    None    => { continue }
+                };
+
+                expr = expr + "\n" + expr_part;
+            }
+        }
+
+        if okay_expr == false {
+            println!("Bad expression");
+            continue
+        }
+
         rust_add_history(expr);
 
         match expr.trim() {
